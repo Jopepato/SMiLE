2514c25bc95c65be6a43c51294cc622a29e2abf9
José Pérez-Parras Toledano <joseperezparras95@gmail.com>
2018-11-02 08:14:41 +0100

    Added test

--- tests/test_smile.py
+++ tests/test_smile.py
@@ -1,7 +1,7 @@
 import pytest
 import numpy as np
 import unittest
-from sklearn.datasets import load_iris
+from sklearn.datasets import load_iris, make_multilabel_classification
 import sys
 from SMILE.smile_ import SMiLE
 
@@ -16,7 +16,20 @@ class SmileTest(unittest.TestCase):
         for i in range(weighted.shape[0]):
             for j in range(weighted.shape[0]):
                 numberOf1s += weighted[i,j]
-        self.assertFalse(numberOf1s == 0)
+        self.assertTrue(numberOf1s != 0)
+
+    def test_label_correlation(self):
+        smile = SMiLE()
+        X, y, p_c, p_w_c = make_multilabel_classification(n_labels=50)
+        correlation = np.zeros(y.shape[1], y.shape[1])
+        correlation = smile.label_correlation(y, smile.s)
+        notEmpty = 0
+        for i in range(correlation.shape[0]):
+            for j in range(correlation.shape[1]):
+                notEmpty += correlation[i,j]
+        self.assertTrue(notEmpty != 0)
+
+
 
 
 if __name__ == '__main__':
